310f9c4248667f6d4b653150d7cb8bc660167993
RossCr0ss <sobechko.r@gmail.com>
2023-03-16 22:38:38 +0200

    Add index js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+  render() {
+    return (
+      <div>
+        {this.state.errorMessage && !this.state.lat && <p>Error: {this.state.errorMessage}</p>}
+        {!this.state.errorMessage && this.state.lat && <p>Latitude: {this.state.lat}</p>}
+        {!this.state.errorMessage && !this.state.lat && <p>Loading...</p>}
+      </div>
+    );
+  }
+}
+
+const root = createRoot(document.querySelector("#root"));
+root.render(<App />);
